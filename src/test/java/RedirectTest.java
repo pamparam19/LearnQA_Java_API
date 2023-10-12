64a3e1aaf9c3521d8651d84eee5fd6de96035cec
pamparam19 <ksenia.sashilina@gmail.com>
2023-10-12 23:38:37 +0300

    add redirectTest

---
+++ src/test/java/RedirectTest.java
@@ -0,0 +1,20 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+public class RedirectTest {
+
+    @Test
+    public void testRedirect(){
+        Response response = RestAssured
+                .given()
+                .redirects()
+                .follow(false)
+                .when()
+                .get("https://playground.learnqa.ru/api/long_redirect")
+                .andReturn();
+
+        String location = response.getHeader("Location");
+        System.out.println(location);
+    }
+}
