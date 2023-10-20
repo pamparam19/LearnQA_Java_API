09980288a6db705cf6178cc43d2c7dd9d27a07ab
pamparam19 <ksenia.sashilina@gmail.com>
2023-10-21 01:01:02 +0300

    add UserAgentTest

---
+++ src/test/java/UserAgentTest.java
@@ -0,0 +1,47 @@
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class UserAgentTest {
+
+    @ParameterizedTest
+    @MethodSource("provideHeadersParameters")
+
+    public void testUserAgent(String userAgent, String expectedPlatform, String expectedBrowser, String expectedDevice){
+        JsonPath response = RestAssured
+                .given()
+                .header("User-Agent", userAgent)
+                .get("https://playground.learnqa.ru/ajax/api/user_agent_check")
+                .jsonPath();
+
+        String platform = response.getString("platform");
+        String browser = response.getString("browser");
+        String device = response.getString("device");
+
+        assertEquals(expectedPlatform, platform, "Platform parameter: '" + platform + "' is unexpected for User-Agent: " + userAgent);
+        assertEquals(expectedBrowser, browser, "Browser parameter: '" + browser + "' is unexpected for User-Agent: " + userAgent);
+        assertEquals(expectedDevice, device, "Device parameter: '" + device + "' is unexpected for User-Agent: " + userAgent);
+    }
+
+    private static Stream<Arguments> provideHeadersParameters(){
+        return Stream.of(
+                Arguments.of("Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30",
+                        "Mobile", "No", "Android"),
+                Arguments.of("Mozilla/5.0 (iPad; CPU OS 13_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/91.0.4472.77 Mobile/15E148 Safari/604.1",
+                        "Mobile", "Chrome", "iOS"),
+                Arguments.of("Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)",
+                        "Googlebot", "Unknown", "Unknown"),
+                Arguments.of("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36 Edg/91.0.100.0",
+                        "Web", "Chrome", "No"),
+                Arguments.of("Mozilla/5.0 (iPad; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1",
+                        "Mobile", "No", "iPhone")
+        );
+    }
+
+}
