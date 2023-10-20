a05e13dabb5d50d28652e80b899adf17bad51325
pamparam19 <ksenia.sashilina@gmail.com>
2023-10-20 17:48:55 +0300

    add HeadersAssertTest fix CookiesAssertTest

--- src/test/java/CookieAssertTest.java
+++ src/test/java/CookieAssertTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Map;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CookieAssertTest {
@@ -17,7 +18,7 @@ public class CookieAssertTest {
         Map<String,String> responseCookies = responseWithCookie.getCookies();
 
         assertTrue(responseCookies.containsKey("HomeWork"), "Response doesn't contain 'HomeWork' cookie");
-        assertTrue(responseCookies.containsValue("hw_value"), "Response cookie doesn't contain 'hw_value'");
+        assertEquals("hw_value", responseCookies.get("HomeWork"), "Response cookie doesn't contain 'hw_value'");
 
     }
 }
---
+++ src/test/java/HeadersAssertTest.java
@@ -0,0 +1,24 @@
+import io.restassured.RestAssured;
+import io.restassured.http.Headers;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class HeadersAssertTest {
+
+    @Test
+    public void testHeadersAssert(){
+        Response responseWithHeaders = RestAssured
+                .get("https://playground.learnqa.ru/api/homework_header")
+                .andReturn();
+
+        Headers headers = responseWithHeaders.getHeaders();
+
+        assertTrue(headers.hasHeaderWithName("x-secret-homework-header"), "Response doesn't contain secret header");
+        assertEquals("Some secret value", headers.getValue("x-secret-homework-header"), "Response" +
+                "doesn't contain header with the secret value");
+
+    }
+}
